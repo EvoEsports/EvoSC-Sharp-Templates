fa764052a6185b7f2b003ab0c126e5a2287bcf8c
snixtho <snixtho@gmail.com>
2022-12-16 15:09:41 +0100

    internal module template

---
+++ EvoScInternalModule/EvoScInternalModule.cs
@@ -0,0 +1,9 @@
+﻿using EvoSC.Modules;
+using EvoSC.Modules.Attributes;
+
+namespace EvoSC.Modules.Official.EvoScInternalModule;
+
+[Module(IsInternal = true)]
+public class EvoScInternalModule : EvoScModule
+{
+}
